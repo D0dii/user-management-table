6845ebc897d074b49bc0d788ae3e6b8253e88ab9
D0dii <marcindolatowski123@gmail.com>
2024-09-16 18:05:21 +0200

    fix: mobile

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { UserManagmentTable } from "@/UserManagmentTable";
 
 function App() {
   return (
-    <div className="px-40 py-7 bg-black text-white">
+    <div className="px-2 py-1 xl:px-24 xl:py-5 2xl:px-32 2xl:py-7 bg-black text-white">
       <UserManagmentTable />
     </div>
   );
